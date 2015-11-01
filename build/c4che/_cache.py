3dfa28529e2a13fc563157205002179f98eb9813
Masahiko Toyoshi <masahiko@Masahiko-no-MacBook-Air.local>
2015-11-01 20:06:34 +0900

    first commit

---
+++ build/c4che/_cache.py
@@ -0,0 +1,6 @@
+BINDIR = '/usr/local/bin'
+DEFINES = ['RELEASE']
+LIBDIR = '/usr/local/lib'
+PEBBLE_SDK_COMMON = '/usr/local/Cellar/pebble-sdk/3.6/Pebble/common'
+PREFIX = '/usr/local'
+TARGET_PLATFORMS = [u'chalk', u'basalt', u'aplite']
