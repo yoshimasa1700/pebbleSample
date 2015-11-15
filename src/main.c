b929a3e2f353882390f4a2dabacbfd7057bb6397
Masahiko Toyoshi <m.yoshi.1700@gmail.com>
2015-11-15 20:01:24 +0900

    アニメーションハンドラの追加

--- src/main.c
+++ src/main.c
@@ -24,6 +24,31 @@ void set_text_layer(){
   text_layer_set_text(text_layer, "Divide each difficulty into as many parts as is feasible and necessary to resolve it.");
 }
 
+// アニメーションが停止した際のハンドラ
+void on_animation_stopped(Animation *anim, bool finished, void *context){
+  // メモリ解放
+  property_animation_destroy((PropertyAnimation*) anim);
+}
+
+void animate_layer(Layer *layer, GRect *start, GRect *finish, int duration, int delay){
+  // アニメーション構造体の宣言
+  PropertyAnimation *anim = property_animation_create_layer_frame(layer, start, finish);
+
+  // アニメの特徴を設定
+  animation_set_duration((Animation*)anim, duration);
+  animation_set_delay((Animation*)anim, delay);
+
+  // アニメが停止した際にメモリを解放するためのハンドラを設定
+  AnimationHandlers handlers = {
+    .stopped = (AnimationStoppedHandler) on_animation_stopped
+  };
+  animation_set_handlers((Animation*)anim, handlers, NULL);
+
+  // アニメーション開始
+  animation_schedule((Animation*)anim);
+}
+
+
 void tick_handler(struct tm *tick_time, TimeUnits units_changed){
   // update window
   strftime(buffer, sizeof("00:00:00"), "%H:%M:%S", tick_time);
